7443361edbd742283ee57d83e4753b96f2e2af26
Eric Maranga <ekirira22@gmail.com>
2023-12-19 09:20:31 +0300

    Completed toy assignment

--- js/speedDetector.js
+++ js/speedDetector.js
@@ -1,26 +1,30 @@
 let carSpeed = parseInt(prompt("Enter the car speed"))
 
-/* We will create a function speedChecker() where if a driver failes this test, he is handed over to the demeriter function */
+/* We will create a function speedChecker() where if a driver exceeds 70 it returns status and if below 70 it says Ok */
+// MAIN LOGIC
 function speedChecker(speed){
-    if(speed >= 70){
-        let points = 0        
-        
-        /* Some Crazy Function Here */
+    if(speed > 70){
+        let overSpeed = speed - 70 
+        const status = Demeriter(overSpeed)
 
-        if(points > 12){
-            return 'License suspended'
-        }else{
-            return points
-        }
+        return status
 
-        /*else block*/
     }else{
         return 'Ok'
     }
 }
 
+/* A function Demeriter that will calculate the points above 70 and return License expired if above 12 points*/
 
-
-// console.log(typeof(speedChecker(90)))
+function Demeriter(speed){
+    const points = Math.floor(speed / 5)
+    //SPEED IS BETWEEN 1-5 // INCREMENTS BY 1
+    //SPEED IS BETWEEN 6-10 // INCREMENT POINTS BY 2
+    if(points <= 12){
+        return `Demerit Points:${points}`
+    }else{
+        return 'License Expired'
+    }    
+}
 
 document.getElementById("demo").innerHTML = ` ${speedChecker(carSpeed)}`
